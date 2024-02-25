476ea103e20363d0f5b081b94e00d4bf5f226790
Jamire44 <2003doyle@gmail.com>
2024-02-25 19:53:11 +0000

    added sql

--- src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -17,8 +20,25 @@ public class ProjectSecurityConfig {
                         .requestMatchers("/contact","/notices").permitAll()).
                 formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
-
         return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("12345")
+                .roles("admin")
+                .build();
+
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("12345")
+                .roles("read")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin,user);
     }
 
 }
