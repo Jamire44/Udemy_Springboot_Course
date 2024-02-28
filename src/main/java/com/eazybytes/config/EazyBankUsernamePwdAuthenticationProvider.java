f71c2b7d11ca5d3ed957222429695209a3ef973d
Jamire44 <2003doyle@gmail.com>
2024-02-28 18:41:49 +0000

    Made a better Authentication Provider for project

---
+++ src/main/java/com/eazybytes/config/EazyBankUsernamePwdAuthenticationProvider.java
@@ -0,0 +1,52 @@
+package com.eazybytes.config;
+
+import com.eazybytes.model.Customer;
+import com.eazybytes.repository.CustomerRepository;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class EazyBankUsernamePwdAuthenticationProvider implements AuthenticationProvider {
+
+    private final CustomerRepository repository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    public EazyBankUsernamePwdAuthenticationProvider(CustomerRepository repository, PasswordEncoder passwordEncoder) {
+        this.repository = repository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String pwd = authentication.getCredentials().toString();
+        List<Customer> customers = repository.findByEmail(username);
+        if (customers.size() > 0){
+            if (passwordEncoder.matches(pwd, customers.get(0).getPwd())){
+                List<GrantedAuthority> authorities = new ArrayList<>();
+                authorities.add(new SimpleGrantedAuthority(customers.get(0).getRole()));
+                return new UsernamePasswordAuthenticationToken(username, pwd, authorities);
+            }else {
+                throw new BadCredentialsException("Invalid Password");
+            }
+        }else {
+            throw new BadCredentialsException("No user registered with details entered");
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
+    }
+}
