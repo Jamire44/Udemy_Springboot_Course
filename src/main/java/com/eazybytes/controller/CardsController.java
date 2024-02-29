ffbf5bafc41bad34285a78bb9657294711fe4823
Jamire44 <2003doyle@gmail.com>
2024-02-29 00:39:53 +0000

    Added Cards controller logic

--- src/main/java/com/eazybytes/controller/CardsController.java
+++ src/main/java/com/eazybytes/controller/CardsController.java
@@ -1,14 +1,30 @@
 package com.eazybytes.controller;
 
+import com.eazybytes.model.Cards;
+import com.eazybytes.repository.CardsRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CardsController {
 
+    private final CardsRepository repository;
+
+    public CardsController(CardsRepository repository) {
+        this.repository = repository;
+    }
+
     @GetMapping("/myCards")
-    public String getLoanDetails(){
-        return "Here are your cards from the DB";
+    public List<Cards> getLoanDetails(@RequestParam int id){
+        List<Cards> cards = repository.findByCustomerId(id);
+        if (cards != null){
+            return cards;
+        } else{
+            return null;
+        }
     }
 
 }
