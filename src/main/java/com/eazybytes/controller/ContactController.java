2178348b33bc978125e243ebcfde831b26bb2985
Jamire44 <2003doyle@gmail.com>
2024-03-01 01:17:49 +0000

    Contact Controller

--- src/main/java/com/eazybytes/controller/ContactController.java
+++ src/main/java/com/eazybytes/controller/ContactController.java
@@ -1,14 +1,27 @@
 package com.eazybytes.controller;
 
+import com.eazybytes.model.Contact;
+import com.eazybytes.repository.ContactRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ContactController {
 
-    @GetMapping("/contact")
-    public String saveContactInquiryDetails(){
-        return "Inquiry details saved to DB";
+    private final ContactRepository repository;
+
+    public ContactController(ContactRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/contact/{id}")
+    public void saveContactInquiryDetails(@RequestBody Contact contact, @PathVariable long id){
+        if(!repository.existsById(id)){
+            repository.save(contact);
+        }
+
     }
 
 }
